05a56ad8d8e11e610ca7e21a298529b1858bf49f
cxk0222 <642192014@qq.com>
2024-08-21 16:07:55 +0800

    extract data write and read logic to storage.js file

---
+++ storage.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs/promises')
+const path = require('path')
+
+// 文件路径
+const DATA_FILE_PATH = path.join(__dirname, 'data', 'todos.json')
+
+console.log('DATA_FILE_PATH', DATA_FILE_PATH)
+
+const readData = async () => {
+  let data
+  try {
+    data = await fs.readFile(DATA_FILE_PATH, { encoding: 'utf8' })
+    data = JSON.parse(data)
+  } catch (error) {
+    return error
+  } finally {
+    return data || []
+  }
+}
+const writeData = async (data) => {
+  try {
+    const toString = JSON.stringify(data, null, 2)
+    await fs.writeFile(DATA_FILE_PATH, toString)
+  } catch (error) {
+    return error
+  }
+}
+
+module.exports = {
+  readData,
+  writeData
+}
