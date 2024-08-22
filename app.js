547b477229ef9f53aabb5f7e3c6efcd6d7d34cc3
cxk0222 <642192014@qq.com>
2024-08-22 19:14:20 +0800

    extract router

--- app.js
+++ app.js
@@ -1,66 +1,10 @@
 const http = require('http')
-const { getAllTodos, getTodoById, createTodo, updateTodo, deleteTodo } = require('./controller.js')
-const { parseRequestBody } = require('./utils.js')
+const router = require('./router.js')
 
-const server = http.createServer(async (req, res) => {
-  const { method, url } = req
+const server = http.createServer(router)
 
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
+const PORT = 3000
 
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200)
-    res.end()
-    return
-  }
-
-  if (method === 'GET' && url === '/todos') {
-    const todos = await getAllTodos()
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(todos))
-  } else if (method === 'POST' && url === '/todos') {
-    const newTodo = await parseRequestBody(req)
-    const createdTodo = await createTodo(newTodo)
-    res.writeHead(201, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(createdTodo))
-  } else if (method === 'GET' && url.match(/\/todos\/\d+/)) {
-    const id = parseInt(url.split('/')[2])
-    const todo = await getTodoById(id)
-    if (todo) {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(todo))
-    } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Todo not found' }))
-    }
-  } else if (method === 'PUT' && url.match(/\/todos\/\d+/)) {
-    const id = parseInt(url.split('/')[2])
-    const upldatedTodo = await parseRequestBody(req)
-    const todo = await updateTodo(id, upldatedTodo)
-    if (todo) {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify(todo))
-    } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Todo not found' }))
-    }
-  } else if (method === 'DELETE' && url.match(/\/todos\/\d+/)) {
-    const id = parseInt(url.split('/')[2])
-    const success = await deleteTodo(id)
-    if (success) {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Todo deleted' }))
-    } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' })
-      res.end(JSON.stringify({ message: 'Todo not found' }))
-    }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ message: 'Route not found' }))
-  }
-})
-
-server.listen(3000, () => {
-  console.log('server is running at 3000')
+server.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`)
 })
---
+++ router.js
@@ -0,0 +1,63 @@
+const { getAllTodos, getTodoById, createTodo, updateTodo, deleteTodo } = require('./controller.js')
+const { parseRequestBody } = require('./utils.js')
+
+const router = async (req, res) => {
+  const { method, url } = req
+
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
+  if (method === 'GET' && url === '/todos') {
+    const todos = await getAllTodos()
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(todos))
+  } else if (method === 'POST' && url === '/todos') {
+    const newTodo = await parseRequestBody(req)
+    const createdTodo = await createTodo(newTodo)
+    res.writeHead(201, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(createdTodo))
+  } else if (method === 'GET' && url.match(/\/todos\/\d+/)) {
+    const id = parseInt(url.split('/')[2])
+    const todo = await getTodoById(id)
+    if (todo) {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(todo))
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Todo not found' }))
+    }
+  } else if (method === 'PUT' && url.match(/\/todos\/\d+/)) {
+    const id = parseInt(url.split('/')[2])
+    const upldatedTodo = await parseRequestBody(req)
+    const todo = await updateTodo(id, upldatedTodo)
+    if (todo) {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify(todo))
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Todo not found' }))
+    }
+  } else if (method === 'DELETE' && url.match(/\/todos\/\d+/)) {
+    const id = parseInt(url.split('/')[2])
+    const success = await deleteTodo(id)
+    if (success) {
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Todo deleted' }))
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Todo not found' }))
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Route not found' }))
+  }
+}
+
+module.exports = router
